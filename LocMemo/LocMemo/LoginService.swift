0b837eef887b440afe447f45f0c9b1d0e5f3d717
Jagaa <jagaa1103@gmail.com>
2015-08-22 17:29:45 +0900

    fixed LoginService
    fixed LoginService. Added completionHandler for async.

--- LocMemo/LocMemo/LoginService.swift
+++ LocMemo/LocMemo/LoginService.swift
@@ -27,39 +27,46 @@ class LoginService {
             if authData != nil {
                 // user authenticated
                 println(authData)
+                completionHandler(res: true)
             } else {
                 // No user is signed in
+                completionHandler(res: false)
             }
         })
     }
     
-    func loginUser(email: String, pass: String){
+    func loginUser(email: String, pass: String, completionHandler:(res:Bool)->()){
         let ref = Firebase(url: Constants.DbUrl)
         ref.authUser(email, password: pass) {
             error, authData in
             if error != nil {
                 // an error occured while attempting login
+                completionHandler(res: false)
             } else {
                 // user is logged in, check authData for data
+                completionHandler(res: true)
             }
         }
     }
     
-    func registerUser(email: String, pass: String){
+    func registerUser(email: String, pass: String, completionHandler:(res:Bool)->()){
         let ref = Firebase(url: Constants.DbUrl)
         ref.createUser(email, password: pass,
             withValueCompletionBlock: { error, result in
                 if error != nil {
                     // There was an error creating the account
+                    completionHandler(res: false)
                 } else {
                     let uid = result["uid"] as? String
                     println("Successfully created user account with uid: \(uid)")
+                    completionHandler(res: true)
                 }
         })
     }
     
-    func logoutUser(){
+    func logoutUser(completionHandler:(res:Bool)->()){
         let ref = Firebase(url: Constants.DbUrl)
         ref.unauth()
+        completionHandler(res: true)
     }
 }
